cc3a6a80776765fac1d804b256063355005676af
Ahmad Hanafi <Contaminated007@gmail.com>
2021-07-09 08:21:43 +0700

    Revert environtment config variable

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
-  KEY: process.env.KEY,
-  BASE_URL: process.env.BASE_URL,
-  BASE_IMAGE_URL: process.env.BASE_IMAGE_URL,
-  CACHE_NAME: process.env.CACHE_NAME,
-  DB_NAME: process.env.DB_NAME,
-  DB_VERSION: process.env.DB_VERSION,
-  OBJECT_STORE_NAME: process.env.OBJECT_STORE_NAME,
+  KEY: '12345',
+  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/',
+  CACHE_NAME: 'EatenIt-v1',
+  DB_NAME: 'eaten-it-db',
+  DB_VERSION: 1,
+  OBJECT_STORE_NAME: 'resto',
 };
 
 export default CONFIG;
